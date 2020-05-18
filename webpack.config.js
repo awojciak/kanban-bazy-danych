bed4e6b74e76f083c6e5ed032e22a95e00722d26
Adrian <adrianwojciak@interia.pl>
2020-05-18 22:13:43 +0200

    refractor of structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.join(__dirname, 'client'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    }
+}
